91f016cedfc3ed187fb8c43f88c88043c93dcdc7
405 <405@academicos.cesde.edu.co>
2022-12-13 11:33:38 -0500

    se agrego todo lo conveniente a ampliar info

--- src/AmpliarInfo/AmpliarInfo.js
+++ src/AmpliarInfo/AmpliarInfo.js
@@ -1,3 +1,4 @@
+import "./AmpliarInfo.css"
 import { useLocation } from "react-router-dom"
 
 export function AmpliarInfo(){
@@ -10,8 +11,40 @@ export function AmpliarInfo(){
 
     return(
         <>
-            <img className="h-100 img-fluid">{producto.foto}</img>
-            <h1>{producto.nombre}</h1>
+            <div className="container">
+                <div className="row my-5">
+                    <div className="col-12 col-md-5 border-end">
+                        <img src={producto.foto} alt="foto" className="img-fluid w-100"/>
+                        <p className="border ">{producto.descripcion}</p>
+                    </div>
+                    <div className="col-12 col-md-4 border p-3">
+                        <h2 className="fw-bold">{producto.nombre}</h2>
+                        <br/>                        
+                        <h1 className="mt-3 display-1">${producto.precio}<span className="descuento">27% off</span></h1>
+                        <br/>
+                        <span className="badgetext-bg-primary">Descuento del dia</span>
+                        <p>Hasta 48 cuotas</p>
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotyja-9de98.appspot.com/o/Mech%2FVisa.png?alt=media&token=ec6d37c2-28fc-4fb7-9ffb-ea8496254bc1" alt="" className="img-fluid icono "/>
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotyja-9de98.appspot.com/o/Mech%2FamericanXpress.png?alt=media&token=e6578c8a-f287-479d-a6e1-0f01f45ed72d" alt="" className="img-fluid ms-2 icono"/>
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotyja-9de98.appspot.com/o/Mech%2FmasterCard.ico?alt=media&token=a7072695-ac3b-4446-941d-01ca762e6c5c" alt="" className="img-fluid ms-2 icono"/>
+                        <br/>
+                        <br/>
+
+                        <div className="row mt-5">
+                            <i className="bi bi-truck fs-1 text-success col-4 icono"></i>
+                            <p className="text-success fw-bold col-8 mt-3">envio gratis a nivel nacional</p>
+                        </div>
+                        <p>Conoce los tiempos y las formas de envío.</p>
+
+                        <div className="row mt-5">
+                            <i class="bi bi-arrow-clockwise fs-1 text-success col-4 icono"></i>
+                            <p className="text-success fw-bold col-8 mt-3">Devolucion gratis.</p>
+                        </div>
+                        <p>30 dias para reembolso.</p>
+
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- src/Mercancia/Mercancia.js
+++ src/Mercancia/Mercancia.js
@@ -24,31 +24,37 @@ export function Mercancia(){
     let Productos=[
         {
             nombre:"Saco original Disturbed character",
+            descripcion:"Saco originial comprado en el concierto emesnemeslefes en Washington d.c, sin usar, talla M 100% algodon bastante guchi. ",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotyja-9de98.appspot.com/o/SacoDisturbed_producto.jpg?alt=media&token=db1892b0-f4e7-47bb-afeb-6e8269fe4bf4",
             precio:40
         },
         {
             nombre:"Mascara edicion Pandemia",
+            descripcion:"Esta mascarilla está hecha con una mezcla de algodón y spandex de alta calidad. Es muy transpirable y elásticoEstá pintado con pintura de tela y una pintura especial sensible a la temperatura que es segura y no tóxicaLa pintura cambia de negro a blanco con el calor de tu aliento, por lo que cuando respiras da la ilusión de que el patrón está cambiando.Ves a través de ranuras cubiertas de malla.Esta máscara funciona mejor a temperatura ambiente promedio o más fría (65-70 grados o menos)Los vendo en Ebay también y tengo un 100% de comentarios y muchos clientes satisfechos. ¡Esta máscara realmente funciona! !Se adapta a casi cualquier persona.",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotyja-9de98.appspot.com/o/MascaraDisturbed.webp?alt=media&token=79db54e7-d761-4093-b7d5-733d032cbc90",
             precio:5
         },
         {
             nombre:"Disco disturbed inside the fire",
+            descripcion:"Evolution es el séptimo álbum de estudio de la banda estadounidense de heavy metal Disturbed. Fue lanzado el 19 de octubre de 2018 por Reprise Records. El primer sencillo del álbum, 'Are You Ready', se lanzó antes que el álbum en agosto de 2018, mientras que el segundo sencillo, 'A Reason to Fight', se lanzó un mes después. El tercer sencillo, 'No More', fue lanzado el 2 de junio de 2019. El álbum debutó en el número cuatro en el Billboard 200.",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotyja-9de98.appspot.com/o/DiscoDisturbed.jpg?alt=media&token=4d0b80b1-06d7-4829-b0b4-6706f225cd7f",
             precio:80
         },
         {
             nombre:"Baso autografiado",
+            descripcion:'¡Vaso de pinta con la sonrisa perturbada de The Guy! Es la mascota de Disturbed y ha aparecido en varios álbumes como el "personaje encapuchado" en la portada del álbum y en videos musicales como el "héroe" que termina derrotando a grandes corporaciones o ejércitos.',
             foto:"https://firebasestorage.googleapis.com/v0/b/spotyja-9de98.appspot.com/o/BasoDisturbed_producto.jpg?alt=media&token=753211bb-1a57-47ec-b2e6-eb7e734e100d",
             precio:100
         },
         {
             nombre:"Gorra Fanmade validada",
+            descripcion:"Disturbed - Sonic Silver Icon & Logo - Gorra de béisbol con licencia oficial. Con un diseño metálico brillante en relieve. Estas gorras de béisbol negras 100% algodón son ajustables en la parte posterior para adaptarse a la mayoría de los tamaños de cabeza. Son perfectos para proteger los ojos del sol en los días soleados y ayudar a mantener el calor cuando hace más frío. ¡También te ayudan a lucir genial!",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotyja-9de98.appspot.com/o/gorraDisturbed.jpeg?alt=media&token=5575d684-717e-4150-9406-8b7547c4e6f4",
             precio:35
         },
         {
             nombre:"Collar militar disturbed premier pass edition",
+            descripcion:"Punk Rock Biker - Joyería gótica de metal pesado imparcial para todas las religiones, colgante de peltre perturbado para hombre, collar unisex de protección amuleto de la suerte medallón talismán con cadena de bola plateada, Metal",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotyja-9de98.appspot.com/o/CollarDisturbed.jpg?alt=media&token=ae72d648-6c05-486b-a146-d75c8bf61aa4",
             precio:60
         }
@@ -95,12 +101,12 @@ export function Mercancia(){
                                 </div>
                             )
                         })
-                    }
-
-                    <Footer/>
+                    }                    
 
                 </div>
             </div>
+
+            <Footer/>
         </>
     )//aqui estoy llamando a las variables es decir interpolar
 }
